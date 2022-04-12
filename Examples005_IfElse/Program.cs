6d1a5ef3d5f6be99f34fb07293626c8a191a473f
Alexander Myagkov <anistonbale@gmail.com>
2022-04-12 20:23:11 +0300

    add Circle if/else

---
+++ Examples005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "александр")
+{
+    Console.WriteLine("Ура, это Санечек!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
