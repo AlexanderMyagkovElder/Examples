c1a59732e0cdc4c1de878ae7626aa28d9944699f
Alexander Myagkov <anistonbale@gmail.com>
2022-04-12 19:51:48 +0300

    random generator

--- Examples004_Div/Program.cs
+++ Examples004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double num1 = 12;
-double num2 = 5;
+﻿double num1 = new Random().Next(1,10);
+Console.WriteLine("Первое число:"+ num1);
+double num2 = new Random().Next(1,10);
+Console.WriteLine("Второе число:"+num2);
 Console.WriteLine(num1/num2);
